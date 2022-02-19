28239e848a1ec38361a800d949fbc040440406bd
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 20:03:33 -0300

    Adicionando na camada MODEL Requisitos

---
+++ Colore/Models/Requisitos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Colore.Models
+{
+    [Table("Requisitos")]
+    public class Requisitos
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string escolaridade { get; set; }
+        
+        [Required]
+        public int experiencia { get; set; }
+        [Required]
+        public string idiomas { get; set; }
+
+        [Required]
+        public string area { get; set; }       
+
+        
+    }
+}
