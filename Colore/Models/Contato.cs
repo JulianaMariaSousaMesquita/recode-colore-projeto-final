57177741d49437fd01822bfea3dfdc1f27d9feda
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 20:04:46 -0300

    Adicionando na camada MODEL Contato

---
+++ Colore/Models/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Colore.Models
+{
+    public class SendEmailDto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+
+        public string? Body { get; set; }
+
+        public string? To { get; set; }
+
+        public string? From { get; set; }
+
+        public string? Subject { get; set; }
+
+        public string? Content { get; set; }
+    }
+}
