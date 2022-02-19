4ceb7142766e2f713145e2df5d4e5e3f01d78400
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 20:02:58 -0300

    Adicionando na camada MODEL Vaga

---
+++ Colore/Models/Vaga.cs
@@ -0,0 +1,28 @@
+﻿using Acessos.Empresa;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Colore.Models
+{
+    [Table("Vaga")]
+    public class Vaga
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string titulo { get; set; }
+        [Required]
+        public string cargo { get; set; }
+        [Required]
+        public double salario { get; set; }
+        [Required]
+        public string descricao { get; set; }
+        [Required]
+        public string beneficios { get; set; }       
+        public DateTime dataFinalInscricao { get; set; }
+        [Required]
+        public Requisitos requisitos { get; set; }
+        [Required]
+        public Empresa empresa { get; set; }
+    }
+}
