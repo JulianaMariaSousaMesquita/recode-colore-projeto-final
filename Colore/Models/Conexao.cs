36a1ae097c8478aefa33a0a4158dc8a9100bf69c
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 20:00:02 -0300

    Adicionando Conexão com banco de dados SQL-Server

---
+++ Colore/Models/Conexao.cs
@@ -0,0 +1,22 @@
+﻿using Acessos.Empresa;
+using Acessos.Usuario;
+using Microsoft.EntityFrameworkCore;
+
+namespace Colore.Models
+{
+    public class Conexao : DbContext
+    {
+        public Conexao(DbContextOptions<Conexao> options) : base(options)
+        {
+
+        }
+        public DbSet<Curriculo> curriculo { get; set; }
+        public DbSet<Acessos> acesso { get; set; }
+        public DbSet<Usuario> usuario { get; set; }
+        public DbSet<Empresa> empresa { get; set; }
+        public DbSet<Vaga> vaga { get; set; }
+        public DbSet<Requisitos> requisitos { get; set; }
+        public DbSet<Endereco> endereco { get; set; }
+        public DbSet<Formacao> formacao { get; set; }
+    }
+}
--- Colore/Program.cs
+++ Colore/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<Conexao>(options => options.UseSqlServer(@"Data Source=DESKTOP-6V7IQ3S;Initial Catalog=Colore;Integrated Security=True"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
