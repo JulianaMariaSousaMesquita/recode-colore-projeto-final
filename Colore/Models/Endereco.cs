11e8f116e31d5678abdb7bef9301f0de8e1853b9
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 20:02:36 -0300

    Adicionando na camada MODEL Endereço

---
+++ Colore/Models/Endereco.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Colore.Models
+{
+    [Table("Endereco")]
+    public class Endereco
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int cep { get; set; }
+
+        [Required]
+        public string rua { get; set; }
+
+        [Required]
+        public int numero { get; set; }
+
+        [Required]
+        public string Complemento { get; set; }
+
+        [Required]
+        public string bairro { get; set; }
+
+        [Required]
+        public string cidade { get; set; }
+
+        [Required]
+        public string uf { get; set; }
+
+        [Required]
+        public string pais { get; set; }
+    }
+}
