9953661b84c90e3a0ec52ebd9cc085354845b53b
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 20:03:14 -0300

    Adicionando na camada MODEL Formação

---
+++ Colore/Models/Formacao.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Colore.Models
+{
+    [Table("Formacao")]
+    public class Formacao
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string titulo { get; set; }
+        [Required]
+        public string nivel { get; set; }
+        [Required]
+        public string instituicao { get; set; }
+        [Required]
+        public DateTime inicio { get; set; }
+        [Required]
+        public DateTime fim { get; set; }
+        [Required]
+        public string? imagemDiploma { get; set; }
+    }
+}
