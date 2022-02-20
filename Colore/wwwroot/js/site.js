0633e0b0e753aa1e96fab298be6f89df3aa94ef5
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-19 22:57:29 -0300

    Atualizando menu

--- Colore/wwwroot/js/site.js
+++ Colore/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#list').click(function (event) { event.preventDefault(); $('#products .item').addClass('list-group-item'); });
+    $('#grid').click(function (event) { event.preventDefault(); $('#products .item').removeClass('list-group-item'); $('#products .item').addClass('grid-group-item'); });
+});
